2f3deb088c240fca7b54f64e20a7facd78cc506e
nupaT <trash_jack@mail.ru>
2021-07-14 22:37:14 +0300

    add: function prev/last days, new style

--- Script.js
+++ Script.js
@@ -1,12 +1,19 @@
 const date = new Date();
-
-// date.setMonth(1);
+// date.setMonth(7);
 // date.setFullYear(2008)
 
 const today = date.getDay();
 const month = date.getMonth();
 const year = date.getFullYear();
 const daysNumber = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+const prevLastDays = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+const firstDayIndex = date.getDay();
+const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+
+const nextDays = 7 - lastDayIndex;
+
+console.log(lastDayIndex);
 
 const daysLong = [
   "",
@@ -40,8 +47,6 @@ document.querySelector(".month").innerHTML = months[month];
 document.querySelector(".year").innerHTML = year;
 document.querySelector(".day").innerHTML = daysLong[today];
 
-// console.log(today);
-
 let weekDays = "";
 for (let w = 0; w < 7; w++) {
   weekDays += `<div>${daysShort[w]}</div>`;
@@ -50,11 +55,27 @@ for (let w = 0; w < 7; w++) {
 
 let monthDays = "";
 
+for (let x = firstDayIndex; x > 0; x--) {
+  monthDays += `<div class="prev__days">${prevLastDays - x + 1}</div>`; //add 0 in front single digit
+}
+
 for (let i = 1; i <= daysNumber; i++) {
-  if (i < 10) {
-    monthDays += `<div>0${i}</div>`;  //add 0 in front single digit
+  if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+    if (i < 10) {
+      monthDays += `<div class="today">0${i}</div>`;
+    } else {
+      monthDays += `<div class="today">${i}</div>`;
+    }
   } else {
-    monthDays += `<div>${i}</div>`;
+    if (i < 10) {
+      monthDays += `<div>0${i}</div>`;
+    } else {
+      monthDays += `<div>${i}</div>`;
+    }
   }
+}
+
+for (let j = 1; j <= nextDays; j++) {
+  monthDays += `<div class="prev__days">${j}</div>`;
   document.querySelector(".date__box").innerHTML = monthDays;
 }
