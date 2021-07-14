c814a88363af002ec9e288b106f34511c53019d0
nupaT <trash_jack@mail.ru>
2021-07-14 22:51:55 +0300

    add: func change to click month, fix some bugs

--- Script.js
+++ Script.js
@@ -1,81 +1,93 @@
 const date = new Date();
-// date.setMonth(7);
-// date.setFullYear(2008)
 
-const today = date.getDay();
-const month = date.getMonth();
-const year = date.getFullYear();
-const daysNumber = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-const prevLastDays = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+const renderCalendar = () => {
+  const today = date.getDay();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  const daysNumber = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+  const prevLastDays = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
 
-const firstDayIndex = date.getDay();
-const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+  const firstDayIndex = date.getDay();
+  const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
 
-const nextDays = 7 - lastDayIndex;
+  const nextDays = 7 - lastDayIndex;
 
-console.log(lastDayIndex);
+  // console.log(lastDayIndex);
 
-const daysLong = [
-  "",
-  "Понедельник",
-  "Вторник",
-  "Среда",
-  "Четверг",
-  "Пятница",
-  "Суббота",
-  "Воскресенье",
-];
+  const daysLong = [
+    "",
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+    "Воскресенье",
+  ];
 
-const daysShort = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+  const daysShort = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
 
-const months = [
-  "Январь",
-  "Февраль",
-  "Март",
-  "Апрель",
-  "Май",
-  "Июнь",
-  "Июль",
-  "Август",
-  "Сентябрь",
-  "Октябрь",
-  "Ноябрь",
-  "Декабра",
-];
+  const months = [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабра",
+  ];
 
-document.querySelector(".month").innerHTML = months[month];
-document.querySelector(".year").innerHTML = year;
-document.querySelector(".day").innerHTML = daysLong[today];
+  document.querySelector(".month").innerHTML = months[month];
+  document.querySelector(".year").innerHTML = year;
+  document.querySelector(".day").innerHTML = daysLong[today];
 
-let weekDays = "";
-for (let w = 0; w < 7; w++) {
-  weekDays += `<div>${daysShort[w]}</div>`;
-  document.querySelector(".week__box").innerHTML = weekDays;
-}
+  let weekDays = "";
+  for (let w = 0; w < 7; w++) {
+    weekDays += `<div>${daysShort[w]}</div>`;
+    document.querySelector(".week__box").innerHTML = weekDays;
+  }
 
-let monthDays = "";
+  let monthDays = "";
 
-for (let x = firstDayIndex; x > 0; x--) {
-  monthDays += `<div class="prev__days">${prevLastDays - x + 1}</div>`; //add 0 in front single digit
-}
+  for (let x = firstDayIndex; x > 0; x--) {
+    monthDays += `<div class="prev__days">${prevLastDays - x + 1}</div>`; //add 0 in front single digit
+  }
 
-for (let i = 1; i <= daysNumber; i++) {
-  if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
-    if (i < 10) {
-      monthDays += `<div class="today">0${i}</div>`;
+  for (let i = 1; i <= daysNumber; i++) {
+    if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+      if (i < 10) {
+        monthDays += `<div class="today">0${i}</div>`;
+      } else {
+        monthDays += `<div class="today">${i}</div>`;
+      }
     } else {
-      monthDays += `<div class="today">${i}</div>`;
-    }
-  } else {
-    if (i < 10) {
-      monthDays += `<div>0${i}</div>`;
-    } else {
-      monthDays += `<div>${i}</div>`;
+      if (i < 10) {
+        monthDays += `<div>0${i}</div>`;
+      } else {
+        monthDays += `<div>${i}</div>`;
+      }
     }
   }
-}
 
-for (let j = 1; j <= nextDays; j++) {
-  monthDays += `<div class="prev__days">${j}</div>`;
-  document.querySelector(".date__box").innerHTML = monthDays;
-}
+  for (let j = 1; j <= nextDays; j++) {
+    monthDays += `<div class="prev__days">${j}</div>`;
+    document.querySelector(".date__box").innerHTML = monthDays;
+  }
+};
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
+
+renderCalendar();
