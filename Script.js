1a0cc81ae3c49a166c824ba951a05dfebfb4a162
nupaT <trash_jack@mail.ru>
2021-07-19 22:34:20 +0300

    fix: no correct date current display; no correct nextDay filling

--- Script.js
+++ Script.js
@@ -4,16 +4,25 @@ const renderCalendar = () => {
   const today = new Date().getDay();
   const month = date.getMonth();
   const year = date.getFullYear();
-  const daysNumber = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+  const daysNumberMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
   const prevLastDays = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
 
-  const firstDayIndex = date.getDay();
+  const firstDayIndex = date.getDay() + 2; //
   const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
 
   const nextDays = 7 - lastDayIndex;
 
-  // console.log(lastDayIndex);
+  const dateString = Intl.DateTimeFormat("ru-RU", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(date);
 
+  console.log(lastDayIndex);
+
+  // !!! переделать - убрать вообще массив и ввытянуть длинное название дня недели с помощью date
   const daysLong = [
     "Воскресенье",
     "Понедельник",
@@ -38,12 +47,15 @@ const renderCalendar = () => {
     "Сентябрь",
     "Октябрь",
     "Ноябрь",
-    "Декабра",
+    "Декабрь",
   ];
 
   document.querySelector(".month").innerHTML = months[month];
   document.querySelector(".year").innerHTML = year;
-  document.querySelector(".day").innerHTML = daysLong[today];
+  document.querySelector(".day").innerHTML = Intl.DateTimeFormat("ru", { weekday: "long" }).format(
+    date
+  ); //daysLong[today];
+  document.querySelector(".scheduleColumn").innerHTML = dateString;
 
   let weekDays = "";
   for (let w = 0; w < 7; w++) {
@@ -54,11 +66,12 @@ const renderCalendar = () => {
   let monthDays = "";
 
   for (let x = firstDayIndex; x > 0; x--) {
-    monthDays += `<div class="prev__days"  id="day__numb">${prevLastDays - x + 1}</div>`; //add 0 in front single digit
+    monthDays += `<div class="prev__days"  id="day__numb">${prevLastDays - x + 1}</div>`;
   }
 
-  for (let i = 1; i <= daysNumber; i++) {
+  for (let i = 1; i <= daysNumberMonth; i++) {
     if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+      //add 0 in front single digit
       if (i < 10) {
         monthDays += `<div class="today"  id="day__numb">0${i}</div>`;
       } else {
